1d11141d8de351825f524abe5262c7d0082deb63
wTrout <will@trout.family>
2021-03-29 20:19:26 -0400

    working with tiny4kOLED library; large 32px high fonts

--- woodStoveSystemATtiny.cpp
+++ woodStoveSystemATtiny.cpp
@@ -1,64 +1,76 @@
 #include <Arduino.h>
-#include <U8x8lib.h>
+#include <Tiny4kOLED.h>
+#include "font16x32digits.h"
 
 constexpr byte therms[] = {A3, A2};
 constexpr byte button   = 1;
 
-U8X8_SSD1306_128X32_UNIVISION_SW_I2C oled(SCL, SDA);
 
 
 //              ┌─────u─────┐
 //          NC -│RST     VCC│- 3.3V
 //              │           │
-//     Therm 1 -│PB3     SCL│- OLED
+//     Therm 1 -│PB3     SCL│- OLED + 4.7k pullup
 //              │           │
-//     Therm 2 -│PB4     PB1│- User Input
+//     Therm 2 -│PB4     PB1│- NC
 //              │           │
-//         GND -│GND     SDA│- OLED
+//         GND -│GND     SDA│- OLED + 4.7k pullup
 //              └───────────┘
-
+//
+// Therm x is AD8495 thermocouple amplifier; T (°C) = (V - 1.25) * 20
+// OLED is ssd1306 with a monochrome 128x32 OLED, 4 pin type (with 3.3v, gnd, SDA and SCL; designed for Raspberry Pi)
 
 void displayTemps(int (&temps)[2]) {
+    byte xPos = 0;
 
-    oled.setCursor(0,1);
-    if(temps[0] <  10) {oled.print(" ");}
-    if(temps[0] < 100) {oled.print(" ");}
+    oled.clear();
+
+    if(temps[0] <  10) {xPos+=16;}
+    if(temps[0] < 100) {xPos+=16;}
+    oled.setCursor(xPos, 0);
     oled.print(temps[0]);
 
-    oled.setCursor(7,1);
-    oled.print(":");
+    xPos = 80;
 
-    oled.setCursor(8,1);
-    if(temps[1] <  10) {oled.print(" ");}
-    if(temps[1] < 100) {oled.print(" ");}
+    if(temps[1] <  10) {xPos+=16;}
+    if(temps[1] < 100) {xPos+=16;}
+    oled.setCursor(xPos, 0);
     oled.print(temps[1]);
 
+    oled.switchFrame();
 }
 
 void readTemps(int (&temps)[2], byte count) {
 
     for(byte i=0; i<2; i++) {
-        temps[i] = 0;
+        float read = 0;
         analogRead(therms[i]);
         delay(5);
 
         for(byte n=0; n<count; n++) {
-            temps[i] += analogRead(therms[i]);
+            read += analogRead(therms[i]);
         }
-        temps[i] /= count;
-        temps[i] *= 20;
-        temps[i] /= 31;
-        temps[i] -= 250;
-        temps[i] = constrain(temps[i], 0, 999);
+        read /= count;
+        read *= 3300;
+        read /= 1023;
+        read -= 1250;
+        read /= 5;
+
+        read = constrain(read, 0, 999);
+        temps[i] = read;
     }
 }
 
 void setup() {
-    oled.begin();
-    oled.setFont(u8x8_font_px437wyse700a_2x2_n);
-    oled.setFlipMode(true);
-}
+    pinMode(5, INPUT_PULLUP);
+    pinMode(1, INPUT_PULLUP);
 
+    oled.begin(128, 32, sizeof(tiny4koled_init_128x32br), tiny4koled_init_128x32br);
+    oled.setRotation(0);
+    oled.setFont(FONT16X32DIGITS);
+    oled.on();
+    oled.switchRenderFrame();
+}
 
 void loop() {
     constexpr int readPeriod = 5000;
@@ -66,7 +78,7 @@ void loop() {
     static int temps[2];
 
     if(millis() - lastRead >= readPeriod) {
-        readTemps(temps, 20);
+        readTemps(temps, 60);
         displayTemps(temps);
         lastRead = millis();
     }
